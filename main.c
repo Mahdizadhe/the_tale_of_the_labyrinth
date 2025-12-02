afd04aaf7a7fdc2f5058e9747cc17ac82f069df1
Mahdizadhe <mohammadmahdizadhe@gmail.com>
2025-12-02 14:59:14 +0330

    create main

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// #include <windows.h>
+// #include <conio.h>
+#include <time.h>
+// #include <ctime>
+
+void clearScreen()
+{
+    system("cls");
+}
+
+int main()
+{
+    int boardGame[12][12], n, m, lightCorePositionX, lightCorePositionY, playerNo, playerPositionX, playerPositionY, hunterNo, hunterPositionX, hunterPositionY, wallNo, wallsPositionX, wallsPositionY;
+
+    for (int i = 0; i < wallNo; i++)
+    {
+        }
+
+    return 0;
+}
