64758cb1a6ea879ed0ac611cb3690cd5fd536ce8
Mahdizadhe <mohammadmahdizadhe@gmail.com>
2025-12-14 23:56:00 +0330

    phase 1

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ void clearScreen()
 
 int main()
 {
+    printf("Please enter n, m, player count, player position, hunter count, hunter position, wall count, wall position");
     int n, m, playerCount, hunterCount, wallsCount;
 
     char map[100][100];
@@ -19,18 +20,20 @@ int main()
     int lightCorePosition[2];   // 1 col for light core and 2 row for X and Y
     int playersPosition[10][2]; // 10 col for playerCount and 2 row for X and Y
     int huntersPosition[10][2]; // 10 col for hunterCount and 2 row for X and Y
-    int wallsPosition[100][3];  // 100 col for wallCount and 1 row X and 1 row for Y and 1 row for position (H or V | 1 => H and 2 => V)
+    int wallsPosition[100][2];  // 100 col for wallCount and 1 row X and 1 row for Y
+
+    int vWall[100][100] = {0}; // this array is for walls between (x) and (x + 1)
+    int hWall[100][100] = {0}; // this array is for walls between (y) nad (y + 1)
 
     int i, j;
 
     scanf("%d %d", &n, &m);
     scanf("%d %d", &lightCorePosition[0], &lightCorePosition[1]);
-    scanf("%d", &playerCount);
 
+    scanf("%d", &playerCount);
     for (i = 0; i < playerCount; i++)
     {
-        scanf("%d", &playersPosition[i][0]);
-        scanf("%d", &playersPosition[i][1]);
+        scanf("%d %d", &playersPosition[i][0], &playersPosition[i][1]);
     }
 
     scanf("%d", &hunterCount);
@@ -39,24 +42,24 @@ int main()
         scanf("%d %d", &huntersPosition[i][0], &huntersPosition[i][1]);
     }
 
-    char wallType;
-
     scanf("%d", &wallsCount);
     for (i = 0; i < wallsCount; i++)
     {
+        char wallType;
         scanf("%d %d %c", &wallsPosition[i][0], &wallsPosition[i][1], &wallType);
         switch (wallType)
         {
         case 'v':
         case 'V':
-            wallsPosition[i][2] = '2';
+            vWall[wallsPosition[i][0]][wallsPosition[i][1]] = 1;
             break;
         case 'h':
         case 'H':
-            wallsPosition[i][2] = '1';
+            hWall[wallsPosition[i][0]][wallsPosition[i][1]] = 1;
             break;
         default:
-            printf("WALL TYPE ERROR\n");
+            printf("Error: wall type is incurrect\n");
+            i--;
         }
     }
 
@@ -64,7 +67,7 @@ int main()
     {
         for (j = 0; j < m; j++)
         {
-            map[i][j] = '.';
+            map[i][j] = ' ';
         }
     }
 
@@ -84,12 +87,37 @@ int main()
 
     for (i = 0; i < n; i++)
     {
+        // ----- Top walls -----
         for (j = 0; j < m; j++)
         {
-            printf("%c", map[i][j]);
+            printf("+");
+            if (i == 0 || vWall[i - 1][j])
+                printf("---");
+            else
+                printf("   ");
         }
-        printf("\n");
+        printf("+\n");
+
+        // ----- Cells + vertical walls -----
+        for (j = 0; j < m; j++)
+        {
+            if (j == 0 || hWall[i][j - 1])
+                printf("|");
+            else
+                printf(" ");
+
+            printf(" %c ", map[i][j]);
+        }
+        printf("|\n");
     }
+
+    // ----- Bottom walls -----
+
+    for (j = 0; j < m; j++)
+    {
+        printf("+---");
+    }
+    printf("+\n");
 
     return 0;
 }
